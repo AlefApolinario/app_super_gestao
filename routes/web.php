fbbddc304f007c1ffbf2192c48c39b6355d0c5ce
Alef Apolinario <alefcristiangomes@gmail.com>
2021-10-12 17:16:21 -0300

    Parametros opcionais e valores padrao

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,15 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
 
 //para interpolar variáveis com string basta usar aspas duplas
-Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}',
-        function(string $nome,string $categoria,string $assunto,string $mensagem){
+//para tornar parâmetros opcionais, basta inserir ? ao final do nome do parâmetro, e atribuir um valor à variável na função de callback
+//Obs: existe uma limitação que não permite que se torne apenas alguns parâmetros opcionais, pois o framework se perde ao sequenciar os
+//parametros, uma oção é tornar os parâmetros opcionais da direta para esquerda, e passar os parâmetros em ordem, da esqueda pra direita
+Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
+        function(string $nome = 'Desconhecido',
+        string $categoria = 'Informação',
+        string $assunto = 'Contato',
+        string $mensagem = 'Mensagem não Informada' 
+        ){
 
     echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
 });
