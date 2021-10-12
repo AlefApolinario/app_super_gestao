3ac8c2ea51164a04c5084cf6df6d38e85b60a72e
Alef Apolinario <alefcristiangomes@gmail.com>
2021-10-12 17:38:42 -0300

    Tratando parametros com expressoes regulares

--- routes/web.php
+++ routes/web.php
@@ -23,16 +23,14 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-//para interpolar variáveis com string basta usar aspas duplas
-//para tornar parâmetros opcionais, basta inserir ? ao final do nome do parâmetro, e atribuir um valor à variável na função de callback
-//Obs: existe uma limitação que não permite que se torne apenas alguns parâmetros opcionais, pois o framework se perde ao sequenciar os
-//parametros, uma oção é tornar os parâmetros opcionais da direta para esquerda, e passar os parâmetros em ordem, da esqueda pra direita
-Route::get('/contato/{nome?}/{categoria?}/{assunto?}/{mensagem?}',
-        function(string $nome = 'Desconhecido',
-        string $categoria = 'Informação',
-        string $assunto = 'Contato',
-        string $mensagem = 'Mensagem não Informada' 
-        ){
-
-    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
-});
+/*Usa-se as regex (expressões regulares) para controlar o que pode ser passado como parâmetro no caso, o parâmetro categoria_id
+só pode receber algarismos de 0 a 9 e o + indica que deve haver pelo menos um valor, o parâmetro nome só recebe letras de A a Z maiusculo ou minusculo, pelo menos uma letra
+caso as condições não sejam atendidas a rota não será chamada, isso evita erros*/ 
+Route::get('/contato/{nome}/{categoria_id}',
+        function(
+            string $nome = 'Desconhecido',
+            int $categoria_id = 1
+        ) {
+            echo "Estamos aqui: $nome - $categoria_id";
+       }   
+    )->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
