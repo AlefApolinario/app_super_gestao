0efe3276104bd10ff8b05386c56c07672c5052fa
Alef Apolinario <alefcristiangomes@gmail.com>
2021-10-19 15:17:26 -0300

    37. Nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,18 @@ use Illuminate\Support\Facades\Route;
     return 'Olá seja bem vindo';
 });*/
 
-//usar comando php artisan route:list para listar as rotas criadas
-Route::get('/', 'PrincipalController@principal');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-Route::get('/contato', 'ContatoController@contato');
-Route::get('/login', function(){return'Login';});
+//Nomear as rotas, para utlizar as funções Helper do laravel, o name funciona como um apelido para a rota, não ligando ela de maneira
+//absoluta onde ela está sendo usada
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){return'Login';})->name('site.login');
 
 //para agrupamento de rotas
 Route:: prefix('/app')->group(function(){
-    Route::get('/clientes', function(){return'Clientes';});
-    Route::get('/fornecedores', function(){return'Fornecedores';});
-    Route::get('/produtos', function(){return'Produtos';});
+    Route::get('/clientes', function(){return'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', function(){return'Fornecedores';})->name('app.fornecedores');
+    Route::get('/produtos', function(){return'Produtos';})->name('app.produtos');
 });
 
 
