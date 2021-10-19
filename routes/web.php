fd654901745929b3417cff1978a99c25a1ed0813
Alef Apolinario <alefcristiangomes@gmail.com>
2021-10-19 14:43:35 -0300

    34. Super Gestão - Criando o menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,10 @@ use Illuminate\Support\Facades\Route;
     return 'Olá seja bem vindo';
 });*/
 
+//usar comando php artisan route:list para listar as rotas criadas
 Route::get('/', 'PrincipalController@principal');
 
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-/*Usa-se as regex (expressões regulares) para controlar o que pode ser passado como parâmetro no caso, o parâmetro categoria_id
-só pode receber algarismos de 0 a 9 e o + indica que deve haver pelo menos um valor, o parâmetro nome só recebe letras de A a Z maiusculo ou minusculo, pelo menos uma letra
-caso as condições não sejam atendidas a rota não será chamada, isso evita erros*/ 
-Route::get('/contato/{nome}/{categoria_id}',
-        function(
-            string $nome = 'Desconhecido',
-            int $categoria_id = 1
-        ) {
-            echo "Estamos aqui: $nome - $categoria_id";
-       }   
-    )->where('categoria_id','[0-9]+')->where('nome','[A-Za-z]+');
