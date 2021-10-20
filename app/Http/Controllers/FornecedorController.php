c2c339afaf94a725b86747161347d8c8e0da6cb8
Alef Apolinario <alefcristiangomes@gmail.com>
2021-10-20 14:23:38 -0300

    45. Blade - @if/@else

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedores.index');
+
+        $fornecedores = ['Fornecedor 1'];
+
+        return view('app.fornecedores.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedores/index.blade.php
+++ resources/views/app/fornecedores/index.blade.php
@@ -1,19 +1,30 @@
 <h3>Fornecedor</h3>
 
-{{ 'Texto de teste' }}
-<?= 'Texto de teste' ?>
-
-{{-- Fica o cometário que será descartado pelo interpretador do blade --}}
-
 @php
-    //Para comentários de uma linha
+/*
+    if(){
 
-    /*
-        Para comentários de multiplas linhas
-     */
+    }elseif(){
 
-     echo 'Texto de teste'
+    }else{
+
+    }*/
 @endphp
+
+{{-- @dd($fornecedores) <- imprime a array na tela, sem o comando @dd apenas variaveis comuns podem ser interpretadas pelo blade com ->{{}} --}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
+
+
+
+
+
 
 
 
