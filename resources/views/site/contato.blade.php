e4c827aa69c05dc5056b59ff4c8d51ae30f51e63
Alef Apolinario <alefcristiangomes@gmail.com>
2021-10-25 16:41:43 -0300

    69.Blade - Enviando parametros para componentes

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -11,7 +11,9 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                @component('site.layouts._components.form_contato')
+                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                    <p>A nossa equipe analisará a sua mensagem e retornará o mais breve possível</p>
+                    <p>Nosso tempo médio de resposta é 48h</p>
                 @endcomponent
             </div>
         </div>
